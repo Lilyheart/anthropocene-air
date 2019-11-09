7ba6e1bef05f70c2bb7f881e034a4cc5200e6957
Lily <hidingfromstats@address.com>
2019-11-09 17:51:14 -0500

    Add Sulfate (Fine)

--- data/-datasets.js
+++ data/-datasets.js
@@ -1,20 +1,30 @@
 parameterDatasets = {
-  NO3f: {
-    name: "Nitrate (Fine)",
-    filename: "data/NO3f.csv",
+  ALf: {
+    name: "Aluminum Fine",
+    filename: "data/ALf.csv",
+    unit: "µg/m<super>3</super> LC",
     percentileBottom: 0,
-    percentileTop: 1.74
+    percentileTop: 0.23
   },
   NH4f: {
     name: "Ammonium Ion (Fine)",
     filename: "data/NH4f.csv",
+    unit: "µg/m<super>3</super> LC",
     percentileBottom: 0,
     percentileTop: 2.98
   },
-  ALf: {
-    name: "Aluminum Fine",
-    filename: "data/ALf.csv",
+  NO3f: {
+    name: "Nitrate (Fine)",
+    filename: "data/NO3f.csv",
+    unit: "µg/m<super>3</super> LC",
     percentileBottom: 0,
-    percentileTop: 0.23
+    percentileTop: 1.74
+  },
+  SO4f: {
+    name: "Sulfate (Fine)",
+    filename: "data/SO4f.csv",
+    unit: "µg/m<super>3</super> LC",
+    percentileBottom: 0,
+    percentileTop: 4.16
   }
 };
